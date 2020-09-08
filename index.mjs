6bff28ae94ed5709ab33f176b8668d2b651152d4
catpea <corporate.miou@gmail.com>
2020-09-08 11:04:53 -0400

    New Release

--- index.mjs
+++ index.mjs
@@ -16,8 +16,7 @@ import kebab from 'lodash/kebabCase.js';
 const options = {
   directory: {description:'Source directory containing the data', value:'src/text'}, // used to get a list of md files
   extension: {description:'Extension of files containing the original data', value:'.html'}, // files to scan
-  output: {description:'Name of the output destination directory', value:'./dist'}, // to save the feed to
-  name: {description:'Name of the feed file directory', value:'content'},
+  output: {description:'Name of the output/destination directory', value:'./dist/content'}, // to save the feed to
   file: {description:'Name of the feed file', value:'feed.json'},
 }
 
@@ -28,9 +27,9 @@ program.version('0.0.1');
 program
   .option('-v, --verbosity', 'increase output verbosity');
 
-for (let [name, option] in options){
-  console.log(`-${name.substr(0,1)}, --${name} <${name}>`, `${option.description}.`);
-
+for (let [name, option] of Object.entries(options)){
+  console.log(`registering ${name}`);
+  program.option(`-${name.substr(0,1)}, --${name} <${name}>`, `${option.description} (${option.value}).`);
 }
 
 program.parse(process.argv);
