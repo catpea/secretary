c301456a83ff0e0c0aa41c85996ce489ff208d27
catpea <corporate.miou@gmail.com>
2020-09-08 11:45:03 -0400

    New Release

--- index.mjs
+++ index.mjs
@@ -12,31 +12,43 @@ import pretty from 'pretty';
 import commander from 'commander';
 import kebab from 'lodash/kebabCase.js';
 
-
-const options = {
-  directory: {description:'Source directory containing the data', value:'src/text'}, // used to get a list of md files
-  extension: {description:'Extension of files containing the original data', value:'.html'}, // files to scan
-  output: {description:'Name of the output/destination directory', value:'./dist/content'}, // to save the feed to
-  file: {description:'Name of the feed file', value:'feed.json'},
-}
-
 const { Command } = commander;
 const program = new Command();
-program.version('0.0.1');
 
-program
-  .option('-v, --verbosity', 'increase output verbosity');
-
-for (let [name, option] of Object.entries(options)){
-  console.log(`registering ${name}`);
-  program.option(`-${name.substr(0,1)}, --${name} <${name}>`, `${option.description} (${option.value}).`);
+const registry = {
+  directory: {description:'source directory containing the data', value:'src/text'},
+  extension: {description:'extension of files containing the original data', value:'.html'},
+  output: {description:'name of the output/destination directory', value:'./dist/content'},
+  file: {description:'name of the feed file', value:'feed.json'},
 }
 
+program.version('0.0.1');
+
+// commander setup
+program.option('-v, --verbosity', 'increase output verbosity');
+
+// NOTE: automatically add registry options as commander command line arguments
+for (let [name, option] of Object.entries(registry)){
+  program.option(`-${name.substr(0,1)}, --${name} <${name}>`, `set ${option.description} (${option.value}).`);
+}
+
+program
+  .command('info', { isDefault: true })
+  .option('-x, --extra-helpful', 'be extra helpful')
+  .description('clone a repository into a newly created directory')
+  .action(function ({extraHelpful}) {
+      console.log('Printing info', extraHelpful);
+  })
+
+
+// process command line
 program.parse(process.argv);
 
-if (program.debug) console.log(program.opts());
+const options = {};
 
-console.log('pizza details:');
-if (program.analyze){
-  console.log(`Directory analysis: ${program.analyze}`);
+// NOTE: create options based on information from registry overriding based on data from command line
+for (let [name, option] of Object.entries(registry)){
+  let value = option.value;
+  if(program[name]) value = program[name];
+  options[name] = value;
 }
